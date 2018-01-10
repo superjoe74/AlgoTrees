dd7bcd1c6990e01edc29baca3a7b610b3da75a40
superjoe74 <johann.h.h@web.com>
2018-01-10 11:01:37 +0100

    changed pt writing

--- src/algTree/PTToFile.java
+++ src/algTree/PTToFile.java
@@ -6,13 +6,10 @@ import java.io.IOException;
 import algTree.PatriciaTree.Node;
 
 public class PTToFile {
-	private int level;
-	private Node leaf;
 
 	public PTToFile(PatriciaTree tree) {
 		try {
-			deepestLeaf(tree.getRoot(), 0);
-
+			// System.out.println(leaf.m_Key);
 			startWriting(tree.getRoot());
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
@@ -36,51 +33,50 @@ public class PTToFile {
 		f.write(s);
 		writeChildren(f, to);
 	}
-	
+
 	private void writeChildren(FileWriter f, Node node) throws IOException {
+		boolean b = false;
 		if (node.m_Left != null || node.m_Right != null) {
 			f.write("\n[");
 			if (node.m_Left != null) {
 				if (node.m_Right != null) {
 					if (node.m_BitPos >= node.m_Left.m_BitPos) {
-						printRefNode(f, node, node.m_Left, ")),");
+						printRefNode(f, node, node.m_Left, ")))])))),");
+						b = true;
 					} else {
-						writeChild(f, node, node.m_Left, "[])))),");
+						writeChild(f, node, node.m_Left, "");
 						if (node.m_BitPos >= node.m_Left.m_BitPos) {
-							printRefNode(f, node, node.m_Left, "))");
+							printRefNode(f, node, node.m_Left, ")))])))),");
+							b = true;
 						} else {
-							if (node.m_Right == leaf) {
-								writeChild(f, node, node.m_Right, "[]");
-							} else {
-								writeChild(f, node, node.m_Right, "");
-							}
+							writeChild(f, node, node.m_Right, "");
 						}
 					}
-					f.write("))))]");
+					if (!b) {
+						f.write("))))]");
+					}
 				} else {
 					if (node.m_BitPos >= node.m_Left.m_BitPos) {
-						printRefNode(f, node, node.m_Left, "))");
+						printRefNode(f, node, node.m_Left, ")))])))),");
+						b = true;
 					} else {
-						if (node.m_Left == leaf) {
-							writeChild(f, node, node.m_Left, "))");
-						} else {
-							writeChild(f, node, node.m_Left, "[]))");
-						}
+						writeChild(f, node, node.m_Left, "");
 					}
-					f.write("))]");
+					if (!b) {
+						f.write("))]");
+					}
 				}
 			} else {
 				if (node.m_Right != null) {
 					if (node.m_BitPos >= node.m_Right.m_BitPos) {
-						printRefNode(f, node, node.m_Right, "))");
+						printRefNode(f, node, node.m_Right, ")))])))),");
+						b = true;
 					} else {
-						if (node.m_Right == leaf) {
-							writeChild(f, node, node.m_Right, "[]))");
-						} else {
-							writeChild(f, node, node.m_Right, "))");
-						}
+						writeChild(f, node, node.m_Right, "");
 					}
-					f.write("))]");
+					if (!b) {
+						f.write("))]");
+					}
 				}
 			}
 		}
@@ -115,16 +111,20 @@ public class PTToFile {
 	}
 
 	private void deepestLeaf(Node node, int lvl) {
-		if (node.m_Left == null && node.m_Right == null
-				&& lvl >= level) {
+		if ((node.m_Left == null || node.m_Left.m_BitPos <= node.m_BitPos)
+				&& (node.m_Right == null || node.m_Right.m_BitPos <= node.m_BitPos) && lvl >= level) {
 			leaf = node;
 			level = lvl;
 		}
-		if (node.m_Left != null && (node.m_BitPos < node.m_Left.m_BitPos)) {
+		if (node.m_Left != null && (node.m_Left.m_BitPos > node.m_BitPos)) {
 			deepestLeaf(node.m_Left, lvl + 1);
 		}
-		if (node.m_Right != null && (node.m_BitPos < node.m_Right.m_BitPos)) {
+		if (node.m_Right != null && (node.m_Right.m_BitPos > node.m_BitPos)) {
 			deepestLeaf(node.m_Right, lvl + 1);
 		}
 	}
+
+	private Node leaf;
+	private int level;
+
 }
